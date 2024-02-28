2fdca977d190487ac3215e57219298d729c1fe13
Jacob Wooldredge <jake.wooldredge@gmail.com>
2024-02-27 23:57:00 -0500

    updated values

--- FaceCardOrNah.py
+++ FaceCardOrNah.py
@@ -5,9 +5,10 @@
 # If correct, the player guesses if the next card is red or black. 
 # If correct, the player guesses the suit of the next card. 
 # If correct, the player answers the final question to win a point: face card or nah?
+# If any of these decisions are not correct, the next player goes (rotational)
+
 
 import random
-
 
 class Card:
     def __init__(self, suit, value):
@@ -19,7 +20,7 @@ class Card:
 
 suits = ["spades", "clubs", "hearts", "diamonds"]
 
-values = range(1,14)
+values = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"]
 
 deck = [Card(suit, value) for suit in suits for value in values]
 print(deck)
