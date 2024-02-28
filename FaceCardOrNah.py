5942f3ca9a87042da745bbf95911935b14f36d6c
Jacob Wooldredge <jake.wooldredge@gmail.com>
2024-02-27 23:15:30 -0500

    Initial python file

---
+++ FaceCardOrNah.py
@@ -0,0 +1,7 @@
+# Face Card or Nah?
+# A card from a full deck of playing cards is placed face up in front of the player. 
+# The player guesses if the next card is higher or lower. 
+# If correct, the player guesses if the next card is in between or outside of the two face up cards. 
+# If correct, the player guesses if the next card is red or black. 
+# If correct, the player guesses the suit of the next card. 
+# If correct, the player answers the final question to win a point: face card or nah?
