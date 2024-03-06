119c29123b1eb8cd8360669205caca5065ceb2fd
Jacob Wooldredge <jake.wooldredge@gmail.com>
2024-03-05 21:06:04 -0500

    update card class to retrieve card values

--- FaceCardOrNah.py
+++ FaceCardOrNah.py
@@ -5,7 +5,7 @@
 # If correct, the player guesses if the next card is red or black. 
 # If correct, the player guesses the suit of the next card. 
 # If correct, the player answers the final question to win a point: face card or nah?
-# If any of these decisions are not correct, the next player goes (rotational)
+# If any of these decisions are not correct, the next player goes (rotational).
 
 
 import random
@@ -14,13 +14,23 @@ class Card:
     def __init__(self, suit, value):
         self.suit = suit
         self.value = value
+        self.val_num = values[value]
 
     def __repr__(self):
         return "{value} of {suit}".format(value=self.value, suit=self.suit)
 
 suits = ["spades", "clubs", "hearts", "diamonds"]
 
-values = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"]
+values = {"ace": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "jack": 11, "queen": 12, "king": 13}
 
 deck = [Card(suit, value) for suit in suits for value in values]
-print(deck)
+
+
+def draw_cards():
+    card_test = [deck.pop(deck.index(random.choice(deck))) for i in range(5)]
+    turn_one = input("Higher or lower? ").lower()
+    if turn_one == "higher":
+        if card_test[0].val_num > 0:
+            print("PASS")
+
+draw_cards()
