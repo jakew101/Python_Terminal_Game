c6e823814edd08f97f62455fe7f26839aa8c5634
Jacob Wooldredge <jake.wooldredge@gmail.com>
2024-03-10 17:13:43 -0400

    added logic for all functions

--- FaceCardOrNah.py
+++ FaceCardOrNah.py
@@ -17,6 +17,7 @@ class Card:
         self.suit = suit
         self.value = value
         self.val_num = values[self.value]
+        self.is_face_card = True if self.val_num in range(11, 14) else False
 
     def __repr__(self):
         return "{value} of {suit}".format(value=self.value, suit=self.suit)
@@ -33,26 +34,45 @@ def draw_cards():
 card_draw = draw_cards()
 
 def first_question():
-    first = input("Higher or lower? ").lower()
-    if first == "higher" and card_draw[1].val_num >= card_draw[0].val_num:
+    first_question = input("Higher or lower? ").lower()
+    if first_question == "higher" and card_draw[1].val_num >= card_draw[0].val_num:
         return True
-    elif first == "lower" and card_draw[1].val_num <= card_draw[0].val_num:
+    elif first_question == "lower" and card_draw[1].val_num <= card_draw[0].val_num:
         return True
     return False
 
 def second_question():
-    return input("In between or outside? ").lower()
+    second_question = input("In between or outside? ").lower()
+    if second_question == "in between" and card_draw[2].val_num in range(card_draw[0].val_num, (card_draw[1].val_num)):
+        return True
+    elif second_question == "outside" and card_draw[2].val_num not in range(card_draw[0].val_num, (card_draw[1].val_num)):
+        return True
+    return False
 
 def third_question():
-    return input("Red or black? ").lower()
+    third_question = input("Red or black? ").lower()
+    if third_question == "red" and card_draw[3].suit == "Diamonds" or "Hearts":
+        return True
+    elif third_question == "black" and card_draw[3].suit == "Spades" or "Clubs":
+        return True
+    return False
 
 def fourth_question():
-    return input("Suit? ").lower()
+    fourth_question = input("Suit? ").lower()
+    if fourth_question == card_draw[4].suit:
+        return True
+    return False
 
 def final_question():
-    return input("Face card or nah? ").lower()
+    final_question = input("Face card or nah? ").lower()
+    if final_question == "face card" and card_draw[5].is_face_card:
+        return True
+    elif final_question == "nah" and not card_draw[5].is_face_card:
+        return True
+    return False
 
 def start():
+    # printing card_draw for testing - remove later
     print(card_draw)
     if first_question():
         if second_question():
@@ -63,4 +83,6 @@ def start():
     else:
         print("FAIL")
 
-start()
+print(card_draw)
+for i in range(card_draw[0].val_num, card_draw[1].val_num):
+    print(i)
